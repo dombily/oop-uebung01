2b91529695ff978d595005b5a5b3a9b1a12402c2
dombily <d.biegner@yahoo.de>
2019-03-25 17:24:45 +0100

    Klasse CharStackImpl wurde geändert

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,24 @@
 package ueb01;
 
 class CharStackImpl implements CharStack {
+   StringBuilder sb = new StringBuilder();
+
+   public void push(char c){
+        sb.append(c);
+    }
+
+    public char pop(){
+        int last = sb.length()-1;
+        char ch = sb.charAt(last);
+        sb.setLength(last);
+        return ch;
+    }
+
+    public int size(){
+        return sb.length();
+    }
+
+
+
+
 }
